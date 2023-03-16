da6833208d52deb1459a9afe21fd30856db8a334
Thiago Souza <thiago29.06@hotmail.com>
2023-03-16 09:50:43 -0300

    feat: requisito 01

--- src/Battle/index.ts
+++ src/Battle/index.ts
@@ -1,4 +1,3 @@
 // import Battle from './Battle';
 
 // export default Battle;
-console.log('refazendo do zero');
---
+++ src/Races/Race.ts
@@ -0,0 +1,23 @@
+export default abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  public get name() {
+    return this._name;
+  }
+
+  public get dexterity() {
+    return this._dexterity;
+  }
+
+  public static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  public abstract get maxLifePoints(): number;
+}
