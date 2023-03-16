2675aeebeb85f127480980dbfd2211bc0420716c
Thiago Souza <thiago29.06@hotmail.com>
2023-03-16 19:32:09 -0300

    fix: requisito 11

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -3,13 +3,13 @@ import Battle from './Battle';
 
 export default class PVP extends Battle {
   constructor(private _fighter1: Fighter, private _fighter2: Fighter) {
-    super(_fighter2);
+    super(_fighter1);
   }
 
   fight(): number {
     while (this._fighter1.lifePoints !== -1
         && this._fighter2.lifePoints !== -1) {
-      if (this._fighter2.lifePoints === -1) break;
+      this._fighter1.attack(this._fighter2);
       this._fighter2.attack(this._fighter1);
     }
     return this.player.lifePoints === -1 ? -1 : 1;
